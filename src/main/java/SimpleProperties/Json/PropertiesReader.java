803f23b602323124431c12e7216d1591ef29899a
leonk <leonkolata@googlemail.com>
2018-05-01 14:00:17 +0200

    Added PropertiesReader

--- src/main/java/SimpleProperties/Json/PropertiesReader.java
+++ src/main/java/SimpleProperties/Json/PropertiesReader.java
@@ -2,7 +2,6 @@ package SimpleProperties.Json;
 
 import com.google.gson.Gson;
 
-import java.io.File;
 import java.io.Reader;
 import java.util.Map;
 
@@ -19,27 +18,53 @@ public class PropertiesReader
 
   private final PropertiesStructure propertiesStructure;
 
+  /**
+   * Creates a PropertiesStructure from JSON.
+   *
+   * @param json JSON as String
+   */
   public PropertiesReader(String json)
   {
     propertiesStructure = GSON.fromJson(json, PropertiesStructure.class);
   }
 
+  /**
+   * Creates a PropertiesStructure from JSON.
+   *
+   * @param json JSON as Reader
+   */
   public PropertiesReader(Reader json)
   {
     propertiesStructure = GSON.fromJson(json, PropertiesStructure.class);
   }
 
+  /**
+   * Returns the name of the given properties.
+   *
+   * @return PropertiesStructure name
+   */
   public String getName()
   {
     return propertiesStructure.getName();
   }
 
+  /**
+   * Returns all the properties as Map.
+   *
+   * @return a Map
+   */
   public Map<String, Object> getProperties()
   {
     return propertiesStructure.getProperties();
   }
 
-  public String asString()
+  /**
+   * Returns the PropertiesStructure as String.
+   *
+   * @return a JSON String
+   */
+  @Override
+  public String toString()
   {
     return propertiesStructure.toString();
   }
